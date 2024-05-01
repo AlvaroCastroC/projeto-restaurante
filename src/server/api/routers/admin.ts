b7d20b74274f4a4b7abb506f13f98410f8c4366b
AlvaroCastroC <alvarocastrocunha14@gmail.com>
2024-05-01 10:05:40 -0300

    Authentication Update

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -5,56 +5,99 @@ import { nanoid } from "nanoid";
 import { getJwtSecretKey } from "@/lib/auth";
 import cookie from "cookie";
 import { TRPCError } from "@trpc/server";
-
+import { Prisma } from "@prisma/client";
+import bcrypt from 'bcrypt'
 
 export const adminRouter = router({
     login: publicProcedure.input(z.object({ email: z.string().email(), password: z.string() })).mutation(async ({ ctx, input }) => {
-        const { res } = ctx
+        const { res, db } = ctx
         const { email, password } = input
+        const dataUserDB = await db.usuario.findFirst({ where: { email } })
 
-        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+        if (dataUserDB) {
             // Autenticação do usuário como admin
+            const senhaHashed = await bcrypt.compare(password, dataUserDB.password)
 
-            const token = await new SignJWT({}).setProtectedHeader({ alg: 'HS256' }).setJti(nanoid()).setIssuedAt().setExpirationTime('1h').sign(new TextEncoder().encode(getJwtSecretKey()))
+            if (senhaHashed) {
 
-            res.setHeader('Set-Cookie', cookie.serialize('user-Token', token, {
-                httpOnly: true,
-                path: '/',
-                secure: process.env.NODE_ENV === "production"
-            }))
-            return { success: true }
+                const token = await new SignJWT({}).setProtectedHeader({ alg: 'HS256' }).setJti(nanoid()).setIssuedAt().setExpirationTime('1h').sign(new TextEncoder().encode(getJwtSecretKey()))
+
+                res.setHeader('Set-Cookie', cookie.serialize('user-Token', token, {
+                    httpOnly: true,
+                    path: '/',
+                    secure: process.env.NODE_ENV === "production"
+                }))
+                return { success: true }
+            }
+
+            throw new TRPCError({
+                code: "UNAUTHORIZED",
+                message: "Senha inválida"
+            })
         }
 
         throw new TRPCError({
             code: "UNAUTHORIZED",
-            message: "Email ou Senha inválidos"
+            message: "Email inválido"
         })
+
+
     }),
+
 
     registerUser: publicProcedure.input(
         z.object({
             name: z.string(),
-            phone: z.string(),
             email: z.string(),
             password: z.string(),
-            keywords: z.array(z.union([z.literal("maca"), z.literal("gato"), z.literal("cachorro")]))
 
         })
     ).mutation(async ({ ctx, input }) => {
-        const { name, email, phone, password, keywords } = input
+        const { name, email, password, } = input
+        const salt = await bcrypt.genSalt(10)
+        const hashaedPassword = await bcrypt.hash(password, salt)
 
-        const createUser = await ctx.db.usuario.create({
-            data: {
-                name,
-                email,
-                password,
-                phone,
-                keywords,
+        try {
+            if (!(ctx.db.$connect))
 
-            },
-        })
-        return createUser
+                return { success: false }
+
+            else
+
+                await ctx.db.usuario.create({
+                    data: {
+                        name,
+                        email,
+                        password: hashaedPassword,
+
+                    },
+                })
+
+            return { success: true }
+
+        } catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                if (e.code === 'P2002') {
+                    throw new TRPCError({
+                        code: "UNAUTHORIZED",
+                        message: "Email já existe"
+                    })
+                }
+
+
+            }
+
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Não foi possível se registrar , erro no servidor!!',
+            })
+
+
+
+
+        }
     }),
 
 })
+
 
