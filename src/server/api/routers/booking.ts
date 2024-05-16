ce6c3e365acccd0a6dced80597d30735792533e4
AlvaroCastroC <alvarocastrocunha14@gmail.com>
2024-05-16 01:31:22 -0300

    add new TRPC db

---
+++ src/server/api/routers/booking.ts
@@ -0,0 +1,97 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+
+export const bookingRouter = router({
+
+
+    bookingCreate: publicProcedure.input(z.object({
+        bookingDate: z.date(),
+        clientName: z.string(),
+        phone: z.string(),
+        email: z.string(),
+        service: z.string()
+
+    })).mutation(async ({ ctx, input }) => {
+        const { db } = ctx
+        const { bookingDate, clientName, phone, email, service
+        } = input
+
+        const dataClient = await db.client.findUnique({ where: { email } })
+
+        if (!db.$connect) throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Falha ao reservar, tente mais tarde!"
+        })
+
+
+        if (!email) throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Por favor, faça seu cadastro primeiro"
+        })
+
+
+        if (dataClient) {
+            if (new Date() > dataClient.bookingDate) {
+                await db.client.delete({
+                    where: { email }
+                })
+            }
+
+            if (dataClient.bookingDate.getDate() === bookingDate.getDate()) {
+                await db.client.update({
+                    where: { email },
+                    data: {
+                        bookingDate,
+                        service
+                    }
+                })
+                return { success: true, message: "Horário remarcado com sucesso." }
+            } else {
+                await db.client.update({
+                    where: { email },
+                    data: {
+                        bookingDate,
+                        service
+                    }
+                })
+                return { success: true, message: "Horário marcado com sucesso." }
+            }
+        } else {
+            await db.client.create({
+                data: {
+                    bookingDate,
+                    clientName,
+                    phone,
+                    email,
+                    service
+                }
+            })
+            return { success: true, message: "Horário agendado com sucesso." }
+        }
+
+
+
+
+
+    }),
+
+
+
+    bookingQuery: publicProcedure.query(async ({ ctx }) => {
+        const { db } = ctx
+
+
+        if (!await db.client.findMany()) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: "Erro no servidor, tente mais tarde!"
+            })
+        }
+
+        const dataClient = await db.client.findMany()
+
+        return dataClient
+    })
+})
