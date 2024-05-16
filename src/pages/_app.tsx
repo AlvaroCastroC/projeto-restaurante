57bc896aa656baa8238c88e8a30301d95dafc5c0
AlvaroCastroC <alvarocastrocunha14@gmail.com>
2024-05-16 01:41:36 -0300

    update features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,54 @@
-import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import { type AppType } from "next/app"
 
-import { api } from "@/utils/api";
+import { api } from "@/utils/api"
 
-import "@/styles/globals.css";
-import "@/styles/header.css";
-import "@/styles/card.css";
+import Header from "@/components/component/Header"
+import Footer from "@/components/component/Footer"
+import { ToastContainer } from "react-toastify"
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { Router } from "next/router"
+import NProgress from "nprogress"
+
+import "@/styles/globals.css"
+import "@/styles/card.css"
 import "@/styles/Calendar.css"
+import "react-toastify/dist/ReactToastify.css"
+import "@/styles/nprogress.css"
+import { theme } from "@/styles/theme"
 
-import Header from "@/components/H_F_/Header";
-import Footer from "@/components/H_F_/Footer";
+NProgress.configure({
+  showSpinner: false,
+  minimum: 0.5
+
+})
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+})
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+})
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+})
 
 
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable} `}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </main>
+    <ChakraProvider theme={theme}>
+      <main >
+
+        <Header />
+        <ToastContainer pauseOnFocusLoss={false} autoClose={2500} limit={1} pauseOnHover={false} />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+
+    </ChakraProvider>
   );
 };
 
