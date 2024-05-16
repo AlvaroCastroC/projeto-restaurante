a32381675868b5bdeb3dc040e3f04f3ee3fac9ff
AlvaroCastroC <alvarocastrocunha14@gmail.com>
2024-05-16 01:34:10 -0300

    first commit

--- src/pages/agendamento.tsx
+++ src/pages/agendamento.tsx
@@ -1,15 +1,38 @@
 import CalendarComponent from "@/components/Calendar";
+import { db } from "@/server/db";
+import { formatISO } from "date-fns";
+import { type NextPage } from "next";
+import { Day } from '@prisma/client'
+import Head from "next/head";
 
-function Agendamento() {
+interface calendarProps {
+    days: Day[]
+    closedDays: string[]
+}
+
+const Agendamento: NextPage<calendarProps> = ({ days, closedDays, }) => {
+
+
+
     return (
         <section className="w-[100%]">
-            <div className="MAX-CONTAINER min-h-[70%] m-auto">
-                <CalendarComponent />
+            <Head>
+                <title>Salão | Agendamento</title>
+            </Head>
 
-
-            </div>
+            <main className="MAX-CONTAINER min-h-[70%] m-auto">
+                <CalendarComponent days={days} closedDays={closedDays} />
+            </main>
         </section>
     )
+}
+
+export async function getServerSideProps() {
+
+    const days = await db.day.findMany()
+    const closedDays = (await db.closedDay.findMany()).map((d) => formatISO(d.date))
+    return { props: { days, closedDays, } }
+
 }
 
 export default Agendamento;
