b7e029ee43d02813a223946364d1fa31db2a5e5c
AlvaroCastroC <alvarocastrocunha14@gmail.com>
2024-05-28 01:04:42 -0300

    update feature

--- src/server/api/routers/adminDashboard.ts
+++ src/server/api/routers/adminDashboard.ts
@@ -3,9 +3,6 @@ import { adminProcedure, router } from "../trpc";
 import { z } from "zod";
 
 
-
-
-
 export const dashboardRouter = router({
     bookingQuery: adminProcedure.query(async ({ ctx }) => {
         const { db } = ctx
@@ -46,12 +43,150 @@ export const dashboardRouter = router({
         })
     }),
 
+
+    allServiceCreate: adminProcedure.input(z.object({
+        service: z.string(),
+        price: z.number()
+    })).mutation(async ({ ctx, input }) => {
+        const { db } = ctx
+        const { service, price } = input
+
+        const isExistService = await db.allServices.findFirst({
+            where: {
+                service
+            }
+        })
+        const isExist = await db.allServices.count()
+        if (!db.$connect) throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Falha ao reservar, tente mais tarde!"
+        })
+
+        if (isExistService && isExist) {
+            return { success: false, message: "Aparentemente este serviço já existe!" }
+        }
+
+        await db.allServices.create({
+            data: {
+                service,
+                price,
+            }
+        })
+
+        return { success: true, message: "Serviço criado." }
+
+
+    }),
+
+
+    allServiceQuery: adminProcedure.query(async ({ ctx }) => {
+        const { db } = ctx
+
+
+        if (!db.$connect) throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Falha ao reservar, tente mais tarde!"
+        })
+
+        const allService = await db.allServices.findMany({
+            orderBy: {
+                service: 'asc'
+            }
+        })
+
+        return allService
+    }),
+
+
+    allServiceUpdate: adminProcedure.input(z.object({
+        id: z.string(),
+        service: z.string(),
+        price: z.number(),
+    })).mutation(async ({ input, ctx }) => {
+        const { db } = ctx
+        const { service, price, id } = input
+
+        const isExistService = await db.allServices.findFirst({
+            where: {
+                service
+            }
+        })
+        if (!db.$connect) throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Falha ao reservar, tente mais tarde!"
+        })
+
+        if (!isExistService) {
+
+            await db.allServices.update({
+                where: { id },
+                data: {
+                    service,
+                    price
+                }
+            })
+
+            return { success: true, message: "Serviço atualizado" }
+
+        }
+
+        return { success: false, message: "Já existe este serviço" }
+
+
+    }),
+
+
+    allServiceDelelete: adminProcedure.input(z.object({
+        id: z.string().optional(),
+        optional: z.enum(["all", "one"])
+    })).mutation(async ({ ctx, input }) => {
+        const { id, optional } = input
+        const { db } = ctx
+
+        const isExists = await db.allServices.count()
+
+        if (!db.$connect()) throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Erro no servidor, tente mais tarde!"
+        })
+
+
+        if (isExists) {
+            if (optional === "all") {
+                await db.allServices.deleteMany()
+
+                return { success: true, message: "Concluido." }
+            } else {
+                await db.allServices.delete({
+                    where: {
+                        id
+                    }
+                })
+
+                return { success: true, message: "Concluido." }
+            }
+        } else {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Serviços inexistentes"
+            })
+        }
+
+
+
+    }),
+
     serviceDelelete: adminProcedure.input(z.object({
         id: z.number()
     })).mutation(async ({ ctx, input }) => {
         const { id } = input
         const { db } = ctx
 
+        const clientService = await db.client.findMany({
+            include: {
+                service: true
+            }
+        })
 
 
         if (!db.$connect()) throw new TRPCError({
@@ -60,12 +195,17 @@ export const dashboardRouter = router({
         })
 
         try {
-            await db.services.delete({
-                where: {
-                    id
-                }
-            })
+            if (clientService) {
+                await db.services.delete({
+                    where: {
+                        id
+                    }
+                })
 
+                return { success: true, message: "cliente excluido com sucesso!" }
+
+
+            }
         } catch (error) {
             throw new TRPCError({
                 code: "NOT_FOUND",
